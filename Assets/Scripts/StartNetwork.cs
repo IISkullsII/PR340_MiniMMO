fa3b99a4d1982c4b60a4e38f16c72d141ad39b27
Marc Hoffmann <m.h.96@live.de>
2025-01-17 12:49:14 +0100

    [+] Added Script to automaticly start Server/Client

---
+++ Assets/Scripts/StartNetwork.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartServer : MonoBehaviour
+{
+    [SerializeField] private bool StartImmediately = false;
+    [SerializeField] private bool IsServer = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (StartImmediately && NetworkManager.Singleton != null)
+        {
+            if (IsServer)
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+            else
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
