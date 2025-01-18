86ddf22fd9cd5b549b51ef9a3393d8e659cf5289
Marc Hoffmann <m.h.96@live.de>
2025-01-18 13:43:27 +0100

    [*] Refactored for new PlayerData

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -6,25 +6,32 @@ using UnityEngine.Assertions;
 public class PlayerState : NetworkBehaviour
 {
     [SerializeField] private TextMeshPro playerNameTMP;
-    public NetworkVariable<PlayerData> PlayerInfo { get; private set; }
+
+    public NetworkVariable<PlayerData> PlayerInfo { get; private set; } = new NetworkVariable<PlayerData>();
 
     public void Awake()
     {
-        PlayerInfo = new NetworkVariable<PlayerData>();
-        PlayerInfo.OnValueChanged += OnPlayerInfoChanged;
-        
         Assert.IsNotNull(playerNameTMP);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        
+        PlayerInfo.OnValueChanged += OnPlayerInfoChanged;
+        playerNameTMP.text = PlayerInfo.Value.PlayerName;
     }
 
     private void OnPlayerInfoChanged(PlayerData previousvalue, PlayerData newvalue)
     {
         //
-        playerNameTMP.text = newvalue.PlayerName;
+        playerNameTMP.text = PlayerInfo.Value.PlayerName;
     }
 
     public void SetPlayerInfo(PlayerData playerInfo)
     {
         PlayerInfo.Value = playerInfo;
+        PlayerInfo.SetDirty(true);
     }
 
     // Start is called before the first frame update
