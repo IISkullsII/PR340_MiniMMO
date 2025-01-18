fc811323234c49057476e4adce40aa7d18510633
Marc Hoffmann <m.h.96@live.de>
2025-01-18 13:43:02 +0100

    [*] String to FixedString64

--- Assets/Scripts/Structs/PlayerData.cs
+++ Assets/Scripts/Structs/PlayerData.cs
@@ -1,23 +1,30 @@
-﻿using Unity.Netcode;
+﻿using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
 public struct PlayerData : INetworkSerializable
 {
     public ulong PlayerID;
-    public string PlayerName;
+    private FixedString64Bytes _playerName;
+
+    public string PlayerName
+    {
+        get => _playerName.ToString();
+        set => _playerName = new FixedString64Bytes(value);
+    }
     public Vector3 PlayerColor;
 
     public PlayerData(ulong playerID, string playerName, Vector3 playerColor)
     {
         PlayerID = playerID;
-        PlayerName = playerName;
+        _playerName = new FixedString64Bytes(playerName);
         PlayerColor = playerColor;
     }
 
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
         serializer.SerializeValue(ref PlayerID);
-        serializer.SerializeValue(ref PlayerName);
+        serializer.SerializeValue(ref _playerName);
         serializer.SerializeValue(ref PlayerColor);
     }
 }
